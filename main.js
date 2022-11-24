f53ddbf007ef84cf4fd3ea7b3b93dc08adc88c48
Peenaksh <85746966+Peenaksh@users.noreply.github.com>
2022-11-24 17:41:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
+leftWrist_x = 0
+rightWrist_x = 0
+difference = 0
 function setup() {
     video = createCapture(VIDEO)
     video.size(550,500)
     poseNet = ml5.poseNet(video,modelloaded)
-    poseNet.on('pose',gotposes)
+    poseNet.on('pose',gotPoses)
 
     canvas= createCanvas(550,500)
     canvas.position(560,150)
@@ -11,8 +14,19 @@ function setup() {
         console.log("Posenet is initialised")
     }
 
+    function draw() {
+        background("#78fa32");
+        textSize(difference);
+        fill("#071026")
+        text('WEBAPP',100,200)
+    }
     function gotPoses(results) {
         if(results.length>0) {
             console.log(results);
+            leftWrist_x = results[0].pose.leftWrist.x
+            rightWrist_x = results[0].pose.rightWrist.x
+            difference = floor(leftWrist_x - rightWrist_x)
+            console.log("the left wrsit x = " + leftWrist_x + "the right wrist x = " + rightWrist_x)
+            
         }
     }
